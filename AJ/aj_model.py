0993e99a1fdd184738e10168c1bb87ba615dcdad
demojuly24gcp <edurekajuly24gcp@gmail.com>
2025-07-19 21:21:01 +0530

    adding dagshub

--- AJ/aj_model.py
+++ AJ/aj_model.py
@@ -3,6 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+import dagshub
+import requests
+import mlflow
 
 # Load the wine quality dataset
 data = pd.read_csv(r'C:\Users\ankit_aj\Desktop\MLOPS-case_studies\Demo_050725_DVC\SKILLFY_190725\data\winequality-red.csv')
@@ -31,3 +34,14 @@ print(f"Accuracy: {accuracy:.4f}")
 print(f"F1-score (weighted): {f1:.4f}")
 print(f"Precision (weighted): {precision:.4f}")
 print(f"Recall (weighted): {recall:.4f}")
+
+dagshub.init(repo_owner='edurekajuly24gcp', repo_name='SKILLFY_190725', mlflow=True)
+
+with mlflow.start_run():
+    mlflow.log_param("model", "RandomForestClassifier")
+    mlflow.log_param("random_state", 42)
+    mlflow.log_param("test_size", 0.2)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("f1_score_weighted", f1)
+    mlflow.log_metric("precision_weighted", precision)
+    mlflow.log_metric("recall_weighted", recall)
